cfbd620530edf6ea0d06eabb2f4649d02a5cdf4b
auldaf <auliadaffarahmani@gmail.com>
2023-10-09 01:16:57 +0700

    Weather Widget

--- Tugas 5/script.js
+++ Tugas 5/script.js
@@ -16,6 +16,7 @@ async function checkWeather(city){
     //buat bikin ke bahasa indonesia ditambahin lang=id
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&lang=id`;
 
+    //ini buat ajax nya
     const weather_data = await fetch(`${url}`).then(response => response.json());
 
 
